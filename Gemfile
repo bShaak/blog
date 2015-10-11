fface2c26fb17b9b5b6d64f505d648ee9d6b8dc9
Braden Shaak <bradenshaak@gmail.com>
2015-10-10 22:26:03 -0700

    added mail_for gem and contact views

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'pygments.rb', '~> 0.6.3'
 gem 'redcarpet', '~> 3.3.3'
 gem 'friendly_id', '~> 5.1.0'
 gem 'will_paginate', '~> 3.0.7'
+gem 'mail_form', '~> 1.5.1'
 
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
---
+++ app/models/contact.rb
@@ -0,0 +1,16 @@
+class Contact < MailForm::Base
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :file,      :attachment => true
+
+  attribute :message
+  attribute :nickname,  :captcha  => true
+
+  def headers
+    {
+      :subject => "Contact Form",
+      :to => "realdannytanner@yahoo.ca",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
